2c576ffd461956b9f477ea8c355619357c901018
K. Arthur Endsley <endsley@umich.edu>
2025-01-07 11:40:40 -0700

    Updated documentation (it's been awhile); had to remove pyl4c.apps.calibration.extensions

--- pyl4c/apps/calibration/extensions/__init__.py
+++
@@ -1,5 +0,0 @@
-'''
-These sub-modules were created to support the calibration of specific L4C
-model variants (which are also implemented in the corresponding sub-module for
-forward-run simulations, `pyl4c.apps.l4c.extensions`).
-'''
